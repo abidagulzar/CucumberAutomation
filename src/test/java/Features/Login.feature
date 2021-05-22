4b8ea95be1eb2e038db87f6d3a43fc17a6ae564f
abida <abidarana94@gmail.com>
2021-05-22 19:57:42 +0500

    reuse function and regular expression

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -2,6 +2,13 @@ Feature: Banking Application Login
   Scenario: Home page default login
 
     Given User is on banking landing page
-    When user login into application with username and password
+    When user login into application with "rana" and password "4321"
     Then Home page is populated
-    And Cards are displayed
+    And Cards displayed are "true"
+
+  Scenario: Home page default login
+
+    Given User is on banking landing page
+    When user login into application with "abida" and password "12345"
+    Then Home page is populated
+    And Cards displayed are "false"
--- src/test/java/stepDefinition/stepdefinition.java
+++ src/test/java/stepDefinition/stepdefinition.java
@@ -1,5 +1,6 @@
 package stepDefinition;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,17 +12,20 @@ public class stepdefinition {
     public void user_is_on_banking_landing_page() {
         System.out.println("Landing page displayed");
     }
-    @When("^user login into application with username and password$")
-    public void user_login_into_application_with_username_and_password() {
-        System.out.println("Enter user name and password in login screen ");
+    @When("user login into application with {string} and password {string}")
+    public void user_login_into_application_with_and_password(String string, String string2){
+        System.out.println(string);
+        System.out.println(string2);
     }
     @Then("^Home page is populated$")
     public void home_page_is_populated() {
         System.out.println("Home page displayed");
+
     }
-    @And("^Cards are displayed$")
-    public void cards_are_displayed() {
+    @And("Cards displayed are {string}")
+    public void cards_are_displayed(String arg1) {
         System.out.println("Cards displayed");
+        System.out.println(arg1);
 
     }
 }
