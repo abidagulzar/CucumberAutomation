90e37ee43f2d37b9feae0009ce39ccf8458abfa6
abida <abidarana94@gmail.com>
2021-05-23 20:31:46 +0500

    parameterization

--- src/test/java/Features/Login.feature
+++ src/test/java/Features/Login.feature
@@ -2,13 +2,37 @@ Feature: Banking Application Login
   Scenario: Home page default login
 
     Given User is on banking landing page
-    When user login into application with "rana" and password "4321"
+    When user login by "rana" and password "4321"
     Then Home page is populated
     And Cards displayed are "true"
 
   Scenario: Home page default login
 
     Given User is on banking landing page
-    When user login into application with "abida" and password "12345"
+    When user login by "abida" and password "12345"
     Then Home page is populated
     And Cards displayed are "false"
+
+  Scenario: Home page default login
+
+    Given User is on banking landing page
+    When user signup with the following detail
+    |abbi|12345|abidarana96@gmail.com|Lahore|
+    Then Home page is populated
+    And Cards displayed are "false"
+
+  Scenario Outline: Home page default login
+    Given User is on banking landing page
+    When user login into application <Username> password <password>
+    Then Home page is populated
+    And Cards displayed are "true"
+    Examples:
+      |Username|password|
+    |user1   |u12345   |
+    |user2   |u12345   |
+    |user3   |u12345   |
+    |user4   |u12345   |
+    |user5   |u12345   |
+
+
+
--- src/test/java/stepDefinition/stepdefinition.java
+++ src/test/java/stepDefinition/stepdefinition.java
@@ -1,10 +1,13 @@
 package stepDefinition;
 
-import io.cucumber.java.PendingException;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import java.util.Collections;
+import java.util.List;
 
 public class stepdefinition {
 
@@ -12,17 +15,30 @@ public class stepdefinition {
     public void user_is_on_banking_landing_page() {
         System.out.println("Landing page displayed");
     }
-    @When("user login into application with {string} and password {string}")
-    public void user_login_into_application_with_and_password(String string, String string2){
+    @When("^user login by \"([^\"]*)\" and password \"([^\"]*)\"$")
+    public void user_login_into_application_with_and_password(String string, String string2) {
         System.out.println(string);
         System.out.println(string2);
+    }
+    @When("^user signup with the following detail$")
+    public void user_signup_with_the_follwing_detail(DataTable data){
+        List<List<String>> obj= Collections.singletonList(data.asList());
+        System.out.println(obj.get(0).get(0));
+        System.out.println(obj.get(0).get(1));
+        System.out.println(obj.get(0).get(2));
+        System.out.println(obj.get(0).get(3));
+    }
+    @When("^user login into application (.+) password (.+)$")
+    public void user_login_into_application(String username, String password){
+        System.out.println(username);
+        System.out.println(password);
     }
     @Then("^Home page is populated$")
     public void home_page_is_populated() {
         System.out.println("Home page displayed");
 
     }
-    @And("Cards displayed are {string}")
+    @And("^Cards displayed are \"([^\"]*)\"$")
     public void cards_are_displayed(String arg1) {
         System.out.println("Cards displayed");
         System.out.println(arg1);
